16ae618e558e4a2b26b01fe91e60dd8693ce65bd
Prajwal-Shetty1 <prajwalshetty7975@gmail.com>
2026-02-22 13:03:45 +0530

    Sixth Commit

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -28,6 +28,22 @@ const Orders = ({ token }) => {
 
   }
 
+  //update Order status for Admin Panel
+  const statusHandler = async (event, orderId) => {
+    try {
+      const response = await axios.post(backendUrl + '/api/order/status', { orderId, status: event.target.value }, { headers: { token } });
+      if (response.data.success) {
+        await fetchAllOrders();
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message)
+    }
+
+  }
+
   useEffect(() => {
     fetchAllOrders();
   }, [token])
@@ -40,17 +56,17 @@ const Orders = ({ token }) => {
           {
             orders.map((order, index) => (
               <div key={index} className="order-card">
-                <img src={assets.parcel_icon} alt="" className="order-icon"/>
+                <img src={assets.parcel_icon} alt="" className="order-icon" />
                 {/* ✅ ORDER ITEMS */}
                 <div className="order-items">
                   {
                     order.item.map((item, i) => (
                       <p key={i} className="order-item">
-                        {item.name}  {" "} × {" "} {item.quantity}<span  className="order-size"> {item.size}</span></p>
+                        {item.name}  {" "} × {" "} {item.quantity}<span className="order-size"> {item.size}</span></p>
                     ))
                   }
                 </div>
-                  {/* USER DETAILS */}
+                {/* USER DETAILS */}
                 <div className="order-user">
                   <p className="user-name"> {order.address.firstname} {order.address.lastname}</p>
                   <p>{order.address.email}</p>
@@ -58,14 +74,15 @@ const Orders = ({ token }) => {
                   <p>{order.address.street}, {order.address.city}</p>
                   <p>{order.address.state}, {order.address.country} - {order.address.zipcode}</p>
                 </div>
-                 {/*ORDER DETAILS*/}
+                {/*ORDER DETAILS*/}
                 <div className="order-details">
                   <p>Items:{order.item.length}</p>
                   <p>PaymentMethod:{order.paymentMethod}</p>
                   <p>Payment:{order.payment ? "Done" : 'Pending'}</p>
                   <p>{new Date(order.date).toLocaleDateString()}</p>
                   <p className="order-amount">{currency}{order.amount}</p>
-                  <select className="order-status-select" value={order.status}>
+                  <select className="order-status-select" value={order.status} 
+                  onChange={(event)=>statusHandler(event,order._id)}>
                     <option value="OrderPlaced">OrderPlaced</option>
                     <option value="Packing">Packing</option>
                     <option value="Shipped">Shipped</option>
